74282f65741411f18db3489ff6b71efe01656c7c
Maftuna Abdullaeva <mafa-694@mail.ru>
2021-10-21 20:32:42 +0200

    handle errors

--- blatt1/commands.py
+++ blatt1/commands.py
@@ -10,7 +10,18 @@ def run(cmd, text):
     else:
         print(f"{text} OK") # on Fail
 
+# check for arguments length
+if len(sys.argv) < 3:
+    print("Es muss mindestens 2 argumenten sein")
+    sys.exit(0)
+
 last = sys.argv[len(sys.argv)-1] # last file as "testbench.vcd"
+
+# check last argument for format
+if last.partition(".")[2] != "vcd":
+    print("Es muss letzte Datei mit der endung .vcd uebergeben werden")
+    sys.exit(0)
+
 for arg in sys.argv[1:len(sys.argv)-1]: # loop from index=1 until last index - 1
     print(f"FILE: {arg}")
     run(f"ghdl -s {arg}", "Syntax−Check") # Syntax−Check
