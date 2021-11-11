7ee0c74eae3c3d54017766b3cb4de04ace680599
Maftuna Abdullaeva <mafa-694@mail.ru>
2021-11-11 23:09:45 +0100

    init 4.1 f

--- blatt4/4.1/function_41.vhdl
+++ blatt4/4.1/function_41.vhdl
@@ -3,16 +3,26 @@ USE IEEE.std_logic_1164.ALL;
 
 ENTITY function_41 IS
     PORT (
---missing
+        A : IN STD_LOGIC;
+        B : IN STD_LOGIC;
+        C : IN STD_LOGIC;
+        y : OUT STD_LOGIC
     );
 END function_41;
 
 ARCHITECTURE rtl OF function_41 IS
 
-    component mux41 is 
-        PORT (--missing);
-    end component mux41;
+    COMPONENT mux41 IS
+        PORT (
+            i1 : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+            i2 : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+            i3 : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+            i4 : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+            sel : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+            y : OUT STD_LOGIC_VECTOR(2 DOWNTO 0)
+        );
+    END COMPONENT mux41;
 
-begin
---missing
-end rtl;
+BEGIN
+
+END rtl;
--- blatt4/4.1/function_81.vhdl
+++ blatt4/4.1/function_81.vhdl
@@ -3,16 +3,26 @@ USE IEEE.std_logic_1164.ALL;
 
 ENTITY function_81 IS
     PORT (
---missing
+        --missing
     );
 END function_81;
 
 ARCHITECTURE rtl OF function_81 IS
 
-    component mux81 is 
-        PORT (--missing);
-    end component mux81;
+    COMPONENT mux81 IS
+        PORT (
+            i1_81 : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+            i2_81 : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+            i3_81 : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+            i4_81 : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+            i5_81 : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+            i6_81 : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+            i7_81 : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+            i8_81 : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+            sel_81 : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+            y_81 : OUT STD_LOGIC_VECTOR(2 DOWNTO 0));
+    END COMPONENT mux81;
 
-begin
---missing
-end rtl;
+BEGIN
+    --missing
+END rtl;
