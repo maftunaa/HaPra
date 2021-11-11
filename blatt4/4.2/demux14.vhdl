118fa727e4e6a2515d956a47923b57515700c952
aneto <anetonischuk@gmail.com>
2021-11-11 22:05:40 +0100

    demux not opening :(

--- blatt4/4.2/demux14.vhdl
+++ blatt4/4.2/demux14.vhdl
@@ -13,5 +13,46 @@ ENTITY demux14 IS
 END demux14;
 
 ARCHITECTURE rtl OF demux14 IS
---missing
+ -- andgate with 3 inputs
+    COMPONENT andgate IS
+    port (
+        input1 : in std_logic;
+        input2 : in std_logic;
+        input3 : in std_logic;
+        and_result : out std_logic
+      );
+    END COMPONENT;
+	
+	    --notgate 
+    COMPONENT notgate IS
+        PORT (
+            input1 : IN STD_LOGIC;
+            not_result : OUT STD_LOGIC
+        );
+    END COMPONENT;
+	
+	--signals
+    SIGNAL not_sel : STD_LOGIC_VECTOR(1 DOWNTO 0);
+    SIGNAL and1 : STD_LOGIC_VECTOR(2 DOWNTO 0);
+    SIGNAL and2 : STD_LOGIC_VECTOR(2 DOWNTO 0);
+    SIGNAL and3 : STD_LOGIC_VECTOR(2 DOWNTO 0);
+    SIGNAL and4 : STD_LOGIC_VECTOR(2 DOWNTO 0);
+	
+	BEGIN
+    --bitwise operation for each logic gate because the input signals are multilple-bit signals
+    not_gate_assignment : FOR i IN 0 TO 1 GENERATE
+        not_output1 : notgate port map(sel(i), not_sel(i));
+    END GENERATE not_gate_assignment;
+    
+    and_gate_assignment : FOR i IN 0 TO 2 GENERATE
+        and_output1 : andgate PORT MAP(not_sel(0), not_sel(1), y0(i), and1(i));
+        and_output2 : andgate PORT MAP(not_sel(0), sel(1), y0(i), and2(i));
+        and_output3 : andgate PORT MAP(sel(0), not_sel(1), y0(i), and3(i));
+        and_output4 : andgate PORT MAP(sel(0), sel(1), y0(i), and4(i));
+    END GENERATE and_gate_assignment;
+    
+	i0 <= and1;
+    i1 <= and2;
+    i2 <= and3;
+    i3 <= and4;
 END rtl;
--- blatt4/4.2/demux14_tb.vhdl
+++ blatt4/4.2/demux14_tb.vhdl
@@ -1,9 +1,45 @@
-library ieee;
-use ieee.std_logic_1164.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
 
-entity demux14_tb is
-end demux14_tb;
+ENTITY demux41_tb IS
+END demux41_tb;
 
-architecture testbench of demux14_tb is
---missing
-end testbench;
+ARCHITECTURE testbench OF demux41_tb IS
+    COMPONENT demux41
+        PORT (
+        y0 : IN std_logic_vector(2 DOWNTO 0); 
+        sel : IN std_logic_vector(1 DOWNTO 0); 
+        i0 : OUT std_logic_vector(2 DOWNTO 0);
+        i1 : OUT std_logic_vector(2 DOWNTO 0);
+        i2 : OUT std_logic_vector(2 DOWNTO 0);
+        i3 : OUT std_logic_vector(2 DOWNTO 0)
+        );
+    END COMPONENT;
+
+    SIGNAL i0, i1, i2, i3, y0 : STD_LOGIC_VECTOR(2 DOWNTO 0);
+    SIGNAL sel : STD_LOGIC_VECTOR(1 DOWNTO 0);
+
+BEGIN
+    demultiplex : demux41 PORT MAP(y0, sel, i0, i1, i2, i3);
+
+    PROCESS BEGIN
+	
+		y0 <= "001";
+        sel <= "00";
+        WAIT FOR 1 ns;
+	
+		y0 <= "001";		
+        sel <= "01";
+        WAIT FOR 1 ns;
+
+		y0 <= "001";		
+        sel <= "10";
+        WAIT FOR 1 ns;
+
+		y0 <= "001";		
+        sel <= "11";
+        WAIT FOR 1 ns;
+
+        WAIT;
+    END PROCESS;
+END testbench;
